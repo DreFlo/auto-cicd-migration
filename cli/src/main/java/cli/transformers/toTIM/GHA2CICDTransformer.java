20db047b03aeec414d7880de432fca69e0cfbe4e
André Flores <andrejesusferflores@gmail.com>
2024-02-21 13:53:13 +0000

    Move transformations project directory

--- cli/src/main/java/cli/transformers/toTIM/GHA2CICDTransformer.java
+++ cli/src/main/java/cli/transformers/toTIM/GHA2CICDTransformer.java
@@ -4,10 +4,8 @@ import d.fe.up.pt.cicd.gha.metamodel.GHA.GHAPackage;
 import d.fe.up.pt.cicd.gha.metamodel.GHA.Workflow;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 
-import java.io.IOException;
-
 public class GHA2CICDTransformer extends ToTIMAbstractTransformer<Workflow, GHAPackage> {
     public GHA2CICDTransformer(ResourceSet resourceSet) {
-        super(resourceSet, GHAPackage.eINSTANCE, "gha2cicd.asm", "GHA");
+        super(resourceSet, GHAPackage.eINSTANCE, "transformations/tsm2tim/gha2cicd.asm", "GHA");
     }
 }
